c75aeb901ce85a2ca927c0f96b94f7896a91efce
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2025-06-24 18:11:34 +0200

    Migrate adm-shame.php to PDO

--- adm-shame.php
+++ adm-shame.php
@@ -1,8 +1,20 @@
 <?php
-include("config.php");
-  $dbres = mysql_query("SELECT *,UNIX_TIMESTAMP(`pc`) AS `pc`,UNIX_TIMESTAMP(`transport`) AS `transport`,UNIX_TIMESTAMP(`bc`) AS `bc`,UNIX_TIMESTAMP(`slaap`) AS `slaap`,UNIX_TIMESTAMP(`kc`) AS `kc`,UNIX_TIMESTAMP(`start`) AS `start`,UNIX_TIMESTAMP(`crime`) AS `crime`,UNIX_TIMESTAMP(`ac`) AS `ac` FROM `users` WHERE `login`='{$_SESSION['login']}'");
-  $data	= mysql_fetch_object($dbres);
-if ($data->level < 200) { exit; }
+
+declare(strict_types=1);
+require 'config.php';
+
+$stmt = pdo_query(
+    "SELECT *,UNIX_TIMESTAMP(`pc`) AS `pc`,UNIX_TIMESTAMP(`transport`) AS `transport`," .
+    "UNIX_TIMESTAMP(`bc`) AS `bc`,UNIX_TIMESTAMP(`slaap`) AS `slaap`," .
+    "UNIX_TIMESTAMP(`kc`) AS `kc`,UNIX_TIMESTAMP(`start`) AS `start`," .
+    "UNIX_TIMESTAMP(`crime`) AS `crime`,UNIX_TIMESTAMP(`ac`) AS `ac` " .
+    "FROM `users` WHERE `login` = ?",
+    [$_SESSION['login']]
+);
+$data = $stmt->fetch();
+if (!$data || $data->level < 200) {
+    exit;
+}
 ?>
 <html>
 <head>
@@ -13,56 +25,68 @@ if ($data->level < 200) { exit; }
 <META name="description" lang="nl" content="Vendetta crimegame met pit.">
 </head>
 <table align=center width=100%>
-  <tr> 
+  <tr>
     <td class="subTitle"><b>Wall Of Shame</b></td>
   </tr>
   <tr><td>&nbsp;&nbsp;</td></tr>
-  <tr> 
+  <tr>
     <td class="mainTxt">
 <?php
-if ($data->level < 1000) {echo"Je hebt niet genoeg rechten";exit;}
-if (isset($_GET['del'])) { mysql_query("DELETE FROM `shame` WHERE `id`='{$_GET['del']}'")or die (mysql_error()); echo "Verwijderd"; }
-
-$dbres	= mysql_query("SELECT `login` FROM `users` WHERE `status`='levend' ORDER BY `login`")or die (mysql_error());
-      echo "<form method=\"post\">Speler: <select name=\"p\">";
-     while($person = mysql_fetch_object($dbres)) {
-        
-        print "	<option value=\"{$person->login}\">{$person->login}</option>\n";}
-        print "  </select> <br><input type=text name=com value=Reden><br><input type=\"submit\" name=\"post\" value=\"Spijker op schandpaal\"></form><br><br>";
-		
-		
-if ($_POST['post']) {
-$vict = mysql_query("SELECT * FROM `shame` WHERE `cheater`='{$_POST['p']}'")or die (mysql_error());
-	$isin = mysql_num_rows($vict);
-if ($isin != 0) { echo "Deze persoon hangt al aan de schandpaal."; }
-elseif ($_POST['com']) { mysql_query("INSERT INTO `shame`(`com`,`time`,`cheater`,`person`) values('{$_POST['com']}',NOW(),'{$_POST['p']}','{$data->login}')")or die (mysql_error()); echo "De persoon is op de schandpaal gespijkerd!"; }
-else { echo "Je moet een bericht opgeven"; }
+if ($data->level < 1000) {
+    echo 'Je hebt niet genoeg rechten';
+    exit;
 }
+if (isset($_GET['del'])) {
+    pdo_query('DELETE FROM `shame` WHERE `id` = ?', [$_GET['del']]);
+    echo 'Verwijderd';
+}
+
+$dbres = pdo_query("SELECT `login` FROM `users` WHERE `status`='levend' ORDER BY `login`");
+echo "<form method=\"post\">Speler: <select name=\"p\">";
+while ($person = $dbres->fetch()) {
+    $login = htmlspecialchars($person->login);
+    print " <option value=\"{$login}\">{$login}</option>\n";
+}
+print "  </select> <br><input type=text name=com value=Reden><br><input type=\"submit\" name=\"post\" value=\"Spijker op schandpaal\"></form><br><br>";
+
+if (isset($_POST['post'])) {
+    $stmt = pdo_query('SELECT * FROM `shame` WHERE `cheater` = ?', [$_POST['p']]);
+    if ($stmt->rowCount() != 0) {
+        echo 'Deze persoon hangt al aan de schandpaal.';
+    } elseif (!empty($_POST['com'])) {
+        pdo_query(
+            'INSERT INTO `shame`(`com`,`time`,`cheater`,`person`) VALUES (?, NOW(), ?, ?)',
+            [$_POST['com'], $_POST['p'], $data->login]
+        );
+        echo 'De persoon is op de schandpaal gespijkerd!';
+    } else {
+        echo 'Je moet een bericht opgeven';
+    }
+}
+
 print <<<ENDHTML
-<table width=100%><tr>	  <td align=center><b>Persoon</b></td> 
-      <td align=center><b>Tijd</b></td> 
+<table width=100%><tr>    <td align=center><b>Persoon</b></td>
+      <td align=center><b>Tijd</b></td>
             <td align=center><b>Reden</b></td>
-			<td align=center><b>Door</b></td>
-			<td align=center><b>Delete</b></td> </tr>
+                        <td align=center><b>Door</b></td>
+                        <td align=center><b>Delete</b></td> </tr>
 
 ENDHTML;
-$query = "SELECT * FROM `shame` ORDER BY `id` ASC"; 
-$info = mysql_query($query) or die(mysql_error()); 
-$count = 0; 
-while ($gegeven = mysql_fetch_object($info)) { 
-$time = $gegeven->time; 
-$cheater = $gegeven->cheater;
-$com = $gegeven->com;
-$door = $gegeven->person;
-$id = $gegeven->id;
-$count++; 
-print <<<ENDHTML
 
-<tr>      	<td align=center>$cheater</td> 
-      <td align=center>$time</td> 
+$info = pdo_query('SELECT * FROM `shame` ORDER BY `id` ASC');
+while ($gegeven = $info->fetch()) {
+    $time = htmlspecialchars($gegeven->time);
+    $cheater = htmlspecialchars($gegeven->cheater);
+    $com = htmlspecialchars($gegeven->com);
+    $door = htmlspecialchars($gegeven->person);
+    $id = htmlspecialchars((string)$gegeven->id);
+    print <<<ENDHTML
+<tr>            <td align=center>$cheater</td>
+      <td align=center>$time</td>
             <td align=center>$com</td>
-			<td align=center>$door</td>
-          	<td align=center><a href=adm-shame.php?del={$id}>[x]</a></td></tr>
+                        <td align=center>$door</td>
+                <td align=center><a href=adm-shame.php?del={$id}>[x]</a></td></tr>
 ENDHTML;
 }
 ?>
+
