846c1a9cb603a7edc6dd656da00d0583fde4558a
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2026-08-05 10:54:47 +0200

    Gevangenistimer in het statuspaneel laten aftellen
    De aftel-JS in assets/js/app.js werkt op elk element met een
    data-tot="<unix-tijd>"-attribuut, zoals de afkoeltimers elders al
    hebben. De celmelding in status_panel() miste dat attribuut, dus
    bleef de timer statisch staan tot een handmatige refresh. Fixes #42.

--- inc/layout.php
+++ inc/layout.php
@@ -551,7 +551,8 @@ function status_panel(array $user): void
     echo '<p class="balklabel">' . $vord . '% naar de volgende rang</p>' . "\n";
 
     if ($cel !== null) {
-        echo '<p class="waarschuwing">Je zit vast: nog ' . e(duration($cel['resterend'])) . '</p>' . "\n";
+        echo '<p class="waarschuwing">Je zit vast: nog <strong data-tot="'
+           . (time() + $cel['resterend']) . '">' . e(duration($cel['resterend'])) . '</strong></p>' . "\n";
     }
 
     // Gezondheid als balk: in één oogopslag zie je of je naar de bloedbank moet.
